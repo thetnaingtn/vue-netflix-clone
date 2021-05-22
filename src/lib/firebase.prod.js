2a0332629791e3eb0b87280dd4ca538f9eaed572
Thet Naing Tun <thetnaingtun@bindez.com>
2021-05-23 02:54:50 +0630

    remove firebase configuration

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -3,12 +3,6 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyB1ToAvkePn77ZwvYedpAR1OkrLEgedsdc",
-    authDomain: "netflix-clone-70c23.firebaseapp.com",
-    projectId: "netflix-clone-70c23",
-    storageBucket: "netflix-clone-70c23.appspot.com",
-    messagingSenderId: "73284308393",
-    appId: "1:73284308393:web:9584df4bab59e89bd548b8"
 };
 
 const firebase = Firebase.initializeApp(config)
